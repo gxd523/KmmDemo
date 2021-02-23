80c85c91f18bd66cfb1c036ff281ce06f158b5f8
gxd <gxd523@gmail.com>
2021-02-23 16:30:41 +0800

    no message

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+}
+rootProject.name = "KmmDemo"
+
+
+include(":androidApp")
+include(":shared")
+
---
+++ shared/src/commonMain/kotlin/com/demo/kmm/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.demo.kmm.shared
+
+expect class Platform() {
+    val platform: String
+}
